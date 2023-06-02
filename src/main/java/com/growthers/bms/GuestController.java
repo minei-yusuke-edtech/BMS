097d23b5d405c511cb69c7f37467237948e8d2d3
Gakiya Ryuto <gakiya.ryuto@edtech.ryukyu>
2023-06-02 14:50:26 +0900

    GuestControllerを追加

---
+++ src/main/java/com/growthers/bms/GuestController.java
@@ -0,0 +1,27 @@
+package com.growthers.bms;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@Controller
+@RequestMapping("guest")
+public class GuestController {
+    
+@GetMapping("view")
+private String profile(Model model) {
+    return "guest/view";
+}
+
+}
