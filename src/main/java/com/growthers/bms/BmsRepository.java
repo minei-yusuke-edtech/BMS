371cf6fe0a92052a7d03b037f4be0eb3b9fbffbb
Gakiya Ryuto <gakiya.ryuto@edtech.ryukyu>
2023-06-06 14:28:26 +0900

    idでデータを取得するメソッドを追加

--- src/main/java/com/growthers/bms/BmsRepository.java
+++ src/main/java/com/growthers/bms/BmsRepository.java
@@ -4,4 +4,8 @@ import java.util.ArrayList;
 
 interface BmsRepository {
     ArrayList<Book> search(SearchForm form);
+    Book findByBookID(int id);
+    void regist(Book BookId, String username);
+    void rentbook(Book book);
+
 }
--- src/main/java/com/growthers/bms/JdbcBmsRepository.java
+++ src/main/java/com/growthers/bms/JdbcBmsRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 class BookRowMapper implements RowMapper<Book> {
     public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Book book = new Book(rs.getString("bookID"), rs.getString("bookTitle"), rs.getString("author"),rs.getString("publisher"),rs.getInt("issue"),rs.getString("version"),rs.getString("isbn"),rs.getString("classCode"),rs.getBoolean("enabled"));
+        Book book = new Book(rs.getInt("bookID"), rs.getString("bookTitle"), rs.getString("author"),rs.getString("publisher"),rs.getInt("issue"),rs.getString("version"),rs.getString("isbn"),rs.getString("classCode"),rs.getBoolean("enabled"));
         return book;
     }
 }
@@ -26,6 +26,27 @@ public class JdbcBmsRepository implements BmsRepository {
       new BookRowMapper(), form.getBookTitle(), form.getClassCode(), form.getAuthor(), form.getPublisher(), form.getISBN());
       return books;
     }
+
+    @Override
+    public Book findByBookID(int id) {
+       ArrayList<Book> books = (ArrayList<Book>) jdbcTemplate.query("SELECT bookID, bookTitle, author, publisher, issue, version, isbn, classCode, enabled FROM books WHERE id = ?",
+       new BookRowMapper(), id);
+       Book book = (books.size() > 0) ? books.get(0) : null; 
+       return book;
+    }
+
+    @Override
+    public void regist(Book BookId, String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'regist'");
+    }
+
+    @Override
+    public void rentbook(Book book) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rentbook'");
+    }
+
 }
     
 
