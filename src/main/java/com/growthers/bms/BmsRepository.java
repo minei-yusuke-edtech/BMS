02ff5fd2f5fd924cf9e6a39fffa8ca72e727df06
Gakiya Ryuto <gakiya.ryuto@edtech.ryukyu>
2023-06-06 16:23:10 +0900

    貸出候補に登録する処理と貸出中図書の情報をとってくる処理と貸出候補図書の情報をとってくる処理のインタフェースを追加

--- src/main/java/com/growthers/bms/BmsRepository.java
+++ src/main/java/com/growthers/bms/BmsRepository.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 interface BmsRepository {
     ArrayList<Book> search(SearchForm form);
     Book findByBookID(int id);
-    void regist(Book BookId, String username);
-    void rentbook(Book book);
-
+    void regist(int bookID, String username);
+    void rentbook(String username);            //貸出中図書をDBから持ってくる用のメソッド
+    void rentCandidate(String username);       //貸出候補図書をDBから持ってくる用のメソッド
 }
