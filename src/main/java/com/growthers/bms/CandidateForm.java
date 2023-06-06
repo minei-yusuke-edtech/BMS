88ccd10a3fced18fa1ba09b9fe70afc0cd4c6bbf
Gakiya Ryuto <gakiya.ryuto@edtech.ryukyu>
2023-06-06 11:03:25 +0900

    CandidateFormを追加

---
+++ src/main/java/com/growthers/bms/CandidateForm.java
@@ -0,0 +1,16 @@
+package com.growthers.bms;
+
+public class CandidateForm {
+    private int bookID;
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    
+
+}
